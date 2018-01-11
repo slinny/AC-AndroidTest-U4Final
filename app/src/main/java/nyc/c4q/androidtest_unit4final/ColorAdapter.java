2521cab8af4636be78acc301fb1f89fce92563bf
slinny <siranli411@gmail.com>
2018-01-10 21:50:47 -0500

    add a toast but it does not work

--- app/src/main/java/nyc/c4q/androidtest_unit4final/ColorAdapter.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/ColorAdapter.java
@@ -1,12 +1,15 @@
 package nyc.c4q.androidtest_unit4final;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,6 +23,7 @@ public class ColorAdapter extends RecyclerView.Adapter<ColorAdapter.ColorViewHol
     private static String TAG = "ColorAdapter";
     private List<String> colorNames;
     private HashMap<String, String> colorDict;
+    private String output;
 
     public ColorAdapter(List<String> colors, HashMap<String, String> colorMap) {
         Sort.selectionSort(colors, true);
@@ -34,7 +38,7 @@ public class ColorAdapter extends RecyclerView.Adapter<ColorAdapter.ColorViewHol
     }
 
     @Override
-    public void onBindViewHolder(ColorViewHolder holder, int position) {
+    public void onBindViewHolder(final ColorViewHolder holder, int position) {
         String color = colorNames.get(position);
         holder.name.setText(color);
         try {
@@ -45,6 +49,14 @@ public class ColorAdapter extends RecyclerView.Adapter<ColorAdapter.ColorViewHol
             // TODO: When the name in a viewHolder is clicked,
             // display a long toast with the text "{color_name} has a HEX value of {color_hex}
             // for example: "blue has a HEX value of #0000ff"
+
+            output = holder.name.getText().toString() + " has a HEX value of " + "";
+            holder.name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(), output, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
