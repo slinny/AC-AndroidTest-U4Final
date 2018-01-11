4a074333e76411b6dd4e18bc57f28f9ce5c9085d
slinny <siranli411@gmail.com>
2018-01-10 21:05:41 -0500

    info option menu but cannot hide and show fragment

--- app/src/main/java/nyc/c4q/androidtest_unit4final/MainActivity.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/MainActivity.java
@@ -1,9 +1,14 @@
 package nyc.c4q.androidtest_unit4final;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private ColorAdapter adapter;
     protected HashMap<String, String> colorDict;
     protected List<String> colorsList;
+    private Menu menu;
+    private final String TAG = getClass().getSimpleName();
+    private InfoFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +44,41 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ColorAdapter(colorsList, colorDict);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     // TODO: Add options menu with the item "Info" which is always visible
     // TODO: When "Info" menu item is clicked, display the fragment InfoFragment
     // TODO: If InfoFragment is already visible and I click "Info", remove InfoFragment from the view.
     // Link to creating options menu: https://github.com/C4Q/AC-Android/tree/v2/Android/Lecture-9-Menus-and-Navigation#anatomy-of-menu-xml
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.info:
+//                showHideFragment(fragment);
+                Log.e(TAG,"Info was clicked");
+                break;
+            default:
+                Log.e(TAG,"empty");
+        }
+        return true;
+    }
+
+//    public void showHideFragment(final InfoFragment fragment) {
+//
+//        FragmentManager fm = getFragmentManager();
+//        fm.beginTransaction()
+//                .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+//                .show(fragment)
+//                .commit();
+//    }
+
 }
