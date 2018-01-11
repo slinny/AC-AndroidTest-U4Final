18d73b8fc5ee53627e037de829c103e5b1941672
slinny <siranli411@gmail.com>
2018-01-10 19:37:14 -0500

    more button in info fragment

--- app/src/main/java/nyc/c4q/androidtest_unit4final/InfoFragment.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/InfoFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by justiceo on 1/9/18.
@@ -12,11 +14,28 @@ import android.view.ViewGroup;
 
 public class InfoFragment extends Fragment {
 
+    Button button;
+    TextView textView;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.info_fragment, container, false);
+        button = v.findViewById(R.id.more_button);
+        textView = v.findViewById(R.id.more_textView);
+        textView.setVisibility(TextView.INVISIBLE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             textView.setVisibility(TextView.VISIBLE);
+             button.setVisibility(button.INVISIBLE);
+            }
+        });
+
         return v;
     }
 }
